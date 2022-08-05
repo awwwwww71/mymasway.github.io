37e92f9c8c6f437f19a941d38c2f69ec22367a07
MasWay <70774862+mymasway@users.noreply.github.com>
2022-08-05 14:43:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,1 @@
-## Installation
-
-_It will install the version of MasWayVPN-STORE™ that is compatible with your VPS's operating system and version._
-
-<p align="center">
-  <img src="https://user-images.githubusercontent.com/76937659/153705486-44e6c1b2-74fa-4d44-be1c-36c8fdb83331.gif"/>
-</p>
-
-```bash
 wget https://raw.githubusercontent.com/mymasway/sc/main/sc.sh && ./sc.sh
-```
-<p align="center">
-  <img src="https://user-images.githubusercontent.com/76937659/153705486-44e6c1b2-74fa-4d44-be1c-36c8fdb83331.gif"/>
-</p>
-
-
-_The version can be manually chosen for your virtual private server if universal installation fails._ See [MasWayVPN Fog Versions_](docs/fog-versions.md).
-##
